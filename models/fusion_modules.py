eb3a5402cf4f6f79ce72375552b846e2744c456b
Rick-Xu315 <2019202366@ruc.edu.cn>
2023-05-15 23:13:37 +0800

    revision of FiLM and Gated
    updated the symmetric version of FiLM and Gated

--- models/fusion_modules.py
+++ models/fusion_modules.py
@@ -32,6 +32,72 @@ class FiLM(nn.Module):
     """
     FiLM: Visual Reasoning with a General Conditioning Layer,
     https://arxiv.org/pdf/1709.07871.pdf.
+    revised for mid-concat case
+    """
+
+    def __init__(self, input_dim=512, dim=512, output_dim=100):
+        super(FiLM, self).__init__()
+
+        self.dim = input_dim
+        self.fc_x = nn.Linear(input_dim, 2 * dim)
+        self.fc_y = nn.Linear(input_dim, 2 * dim)
+        self.fc_out = nn.Linear(2*dim, output_dim)
+
+        self.x_film = x_film
+
+    def forward(self, x, y):
+
+        gamma_x, beta_x = torch.split(self.fc_x(x), self.dim, 1)
+        gamma_y, beta_y = torch.split(self.fc_y(y), self.dim, 1)
+
+        x_new = gamma_y * x + beta_y
+        y_new = gamma_x * y + beta_x
+
+        output = torch.cat((x_new, y_new), dim=1)
+        output = self.fc_out(output)
+
+        return x_new, y_new, output
+    
+
+class GatedFusion(nn.Module):
+    """
+    Efficient Large-Scale Multi-Modal Classification,
+    https://arxiv.org/pdf/1802.02892.pdf.
+    revised for mid-concat case
+    """
+
+    def __init__(self, input_dim=512, dim=512, output_dim=100):
+        super(GatedFusion, self).__init__()
+
+        self.fc_x = nn.Linear(input_dim, dim)
+        self.fc_y = nn.Linear(input_dim, dim)
+        self.fc_out = nn.Linear(2*dim, output_dim)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x, y):
+        out_x = self.fc_x(x)
+        out_y = self.fc_y(y)
+
+       
+        gate_x = self.sigmoid(out_x)
+        y_new = torch.mul(gate_x, out_y)
+
+        gate_y = self.sigmoid(out_y)
+        x_new = torch.mul(gate_y, out_x)
+
+        output = torch.cat((x_new,y_new),dim=1)
+        output = self.fc_out(output)
+
+        return x_new, y_new, output
+    
+
+# the original version of non-symmetric FiLM and Gated
+
+class FiLM_original(nn.Module):
+    """
+    FiLM: Visual Reasoning with a General Conditioning Layer,
+    https://arxiv.org/pdf/1709.07871.pdf.
+    
     """
 
     def __init__(self, input_dim=512, dim=512, output_dim=100, x_film=True):
@@ -58,13 +124,9 @@ class FiLM(nn.Module):
         output = self.fc_out(output)
 
         return x, y, output
-
-
-class GatedFusion(nn.Module):
-    """
-    Efficient Large-Scale Multi-Modal Classification,
-    https://arxiv.org/pdf/1802.02892.pdf.
-    """
+    
+class GatedFusion_original(nn.Module):
+    
 
     def __init__(self, input_dim=512, dim=512, output_dim=100, x_gate=True):
         super(GatedFusion, self).__init__()
